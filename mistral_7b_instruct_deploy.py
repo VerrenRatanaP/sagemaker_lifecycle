8a696ae209b85d6c142843eed24706e21590b1df
VerrenRatanaP <68214474+VerrenRatanaP@users.noreply.github.com>
2024-03-25 11:54:12 +1100

    Update mistral_7b_instruct_deploy.py

--- mistral_7b_instruct_deploy.py
+++ mistral_7b_instruct_deploy.py
@@ -1,13 +1,22 @@
 # deploy_model.py
 import os
+import json
 from sagemaker.jumpstart.model import JumpStartModel
 
 model_id = "huggingface-llm-mistral-7b-instruct"
 model = JumpStartModel(model_id=model_id)
 
+
+def get_notebook_name():
+    log_path = "/opt/ml/metadata/resource-metadata.json"
+    with open(log_path, "r") as logs:
+        _logs = json.load(logs)
+    return _logs["ResourceName"]
+
+
 # Constructing endpoint name from the instance name
-instance_name = os.environ.get("AWS_SAGEMAKER_NOTEBOOK_INSTANCE_NAME", "default")
-endpoint_name = f"{instance_name}_mistral7binstruct_model"
+instance_name = get_notebook_name()
+endpoint_name = f"{instance_name}-model"
 
 # Deploying the model
 predictor = model.deploy(endpoint_name=endpoint_name)
